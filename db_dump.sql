7d918efbcb194cb51e2373c784b95494e48ff677
alokiksingh1 <101609750+alokiksingh1@users.noreply.github.com>
2024-05-09 16:10:35 -0600

    Add files via upload

---
+++ db_dump.sql
@@ -0,0 +1,3 @@
+-- SELECT SUM(sales.transactions.sales_amount) FROM sales.transactions INNER JOIN sales.date ON sales.transactions.order_date=sales.date.date where sales.date.year="2020" and sales.transactions.market_code="Mark001";
+
+SELECT distinct product_code FROM sales.transactions where market_code="Mark001";
